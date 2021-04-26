2969499d5c912d54b71330c17cebd3b803fb1cd2
prerak2022 <81256906+prerak2022@users.noreply.github.com>
2021-04-26 15:59:10 +0530

    Update main.js

--- public_html/main.js
+++ public_html/main.js
@@ -18,7 +18,7 @@
     var hash = this.hash;
   
     // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    // The optional number (800) specifie the number of milliseconds it takes to scroll to the specified area
     $('html, body').animate({
       scrollTop: $(hash).offset().top
     }, 800, function(){
