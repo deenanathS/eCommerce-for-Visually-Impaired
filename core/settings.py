1ce9340d41aed1c2b39e357b8c7c4f81355fe2c7
prerak2022 <81256906+prerak2022@users.noreply.github.com>
2021-05-07 19:03:03 +0530

    updated host email

--- core/settings.py
+++ core/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '###################'
-EMAIL_HOST_PASSWORD = '####################'
+EMAIL_HOST_USER = 'prerakanand.1000@gmail.com'
+EMAIL_HOST_PASSWORD = '1234,prerak'
